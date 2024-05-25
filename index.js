3ee55c5f740792e5d678b9b4b7ebfba97f99a28e
ImArnoldDCODES <danielsarnold010@gmail.com>
2024-05-25 13:39:36 +0100

    hamburger and shadow

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("hello world")
+document.addEventListener('DOMContentLoaded', () => {
+    const hamburgerMenu = document.querySelector('.hamburger-menu');
+    const shadowElement = document.querySelector('.shadow');
+    const modal = document.querySelector('.nav-modal')
+
+    hamburgerMenu.addEventListener('click', () => {
+        shadowElement.classList.toggle('shadow-active');
+        modal.classList.toggle('nav-modal-active');
+    });
+});
