a8bb8ea39e42e61903f2e1d161fedf3954ea603a
ImArnoldDCODES <danielsarnold010@gmail.com>
2024-05-26 14:29:30 +0100

    all

--- index.js
+++ index.js
@@ -4,26 +4,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const shadowElement = document.querySelector('.shadow');
     const modal = document.querySelector('.nav-modal');
 
-    const logoInput = document.getElementById('logo-upload');
-    const logoButton = document.getElementById('logo-button');
-    const logoFilename = document.getElementById('logo-filename');
-
-    logoButton.addEventListener('click', () => {
-        logoInput.click();
-    });
-
-    logoInput.addEventListener('change', () => {
-        const file = logoInput.files[0];
-        if (file) {
-            logoFilename.textContent = file.name;
-            logoButton.textContent = 'Change Logo';
-        } else {
-            logoFilename.textContent = '';
-            logoButton.textContent = 'No Logo';
-        }
-    });
-
     hamburgerMenu.addEventListener('click', () => {
+        console.log('hamburgerMenu clicked');
         shadowElement.classList.toggle('shadow-active');
         modal.classList.toggle('nav-modal-active');
         hamburgerMenu.style.display = 'none';
@@ -31,6 +13,7 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     closeMenu.addEventListener('click', () => {
+        console.log('closeMenu clicked'); 
         shadowElement.classList.toggle('shadow-active');
         modal.classList.toggle('nav-modal-active');
         closeMenu.style.display = 'none';
