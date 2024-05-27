719613d9b57c05692269a921ab9df4f73611f3c6
Frokesy <akindeleayanfeoluwa@gmail.com>
2024-05-27 11:36:21 +0100

    fixed

--- extra.js
+++ extra.js
@@ -2,14 +2,39 @@ document.addEventListener('DOMContentLoaded', () => {
     const submitButton = document.getElementById('submitform');
     const extraDiv = document.querySelector('.extra');
     const form = document.querySelector('.company-form');
+    const modal = document.querySelector('.overlay');
+    const identityModal = document.querySelector('.identity-overlay')
 
     const logoInput = document.getElementById('logo-upload');
     const logoButton = document.getElementById('logo-button');
     const logoFilename = document.getElementById('logo-filename');
+    const employment = document.getElementById('employment');
+    const identity = document.getElementById('identity')
 
     logoButton.addEventListener('click', () => {
         logoInput.click();
     });
+
+    employment.addEventListener('click', () => {
+        modal.classList.toggle('active')
+    })
+
+    modal.addEventListener('click', () => {
+        if (modal.classList.value === 'overlay active') {
+            modal.classList.remove('active')
+        }
+    })
+
+    identity.addEventListener('click', () => {
+        console.log("identity clicked")
+        identityModal.classList.toggle('active')
+    })
+
+    identityModal.addEventListener('click', () => {
+        if (identityModal.classList.value === 'identity-overlay active') {
+            identityModal.classList.remove('active')
+        }
+    })
 
     logoInput.addEventListener('change', () => {
         const file = logoInput.files[0];
@@ -25,7 +50,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
 
     submitButton.addEventListener('click', () => {
-        console.log('hello');
         extraDiv.style.display = 'block'
         form.style.display = 'none'
     });
